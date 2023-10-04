92d6d95d5cafdb5e8b434ab7b21d3441e50ee29d
ingridmartsanchez1996 <ingridmartsanchez4@gmail.com>
2023-10-03 23:29:48 -0600

    Adding more content to AboutMe

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -2,10 +2,40 @@ import './AboutMe.css'
 
 const AboutMe = () => {
     return (
-        <div className='information'>
-            <h2>My name is Ingrid Martinez</h2>
-            <p> I'm 27 years old and I live in Mexico City</p>
+        <div className='resume--container'>
+            <div className='header'>
+                <img src="../../public/images/me.jpg" alt="Ingrid Martinez" />
+                <div>
+                    <h3>Ingrid Jeraldin Martinez Sanchez</h3>
+                    <p>
+                        I am a software developer who is always eager to learn new technologies. I am a team player, patient, and organized.
+                    </p>
+                </div>
+            </div>
+
+            <div className='contact'>
+                <h2>Contact information:</h2>
+                <h4>ingridmartsanchez4@gmail.com</h4>
+                <p>Mexico City</p>
+                <p>Age: 27</p>
+            </div>
+
+            <div className='skills'>
+                <h2>Skills:</h2>
+                <ul>
+                    <li>React</li>
+                    <li>JavaScript</li>
+                    <li>Python</li>
+                    <li>CSS</li>
+                    <li>HTML</li>
+                    <li>GitHub</li>
+                    <li>Jira</li>
+                    <li>Figma</li>
+                </ul>
+            </div>
         </div>
+
+
     )
 }
 
