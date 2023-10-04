cfebe321622d099c6e5c588d3eb441fb167abdd0
ingridmartsanchez1996 <ingridmartsanchez4@gmail.com>
2023-10-03 22:29:11 -0600

    Initial changes.

---
+++ src/Components/AboutMe.jsx
@@ -0,0 +1,12 @@
+import './AboutMe.css'
+
+const AboutMe = () => {
+    return (
+        <div className='information'>
+            <h2>My name is Ingrid Martinez</h2>
+            <p> I'm 27 years old and I live in Mexico City</p>
+        </div>
+    )
+}
+
+export default AboutMe;
